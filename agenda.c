21871f06a4a9c903c632658cfce28cb71258ff17
Mathieu Gagnon <mathieu@gagnon.name>
2018-05-30 14:34:51 -0400

    Progress on agenda.c

--- agenda.c
+++ agenda.c
@@ -1,66 +1,56 @@
-#include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <inttypes.h>
+#include <limits.h>
 #include <errno.h>
 
 #define DAYS(n) 60*60*24*n
-#define BUF_SIZE 11
+#define DAYS_MAX INT_MAX/60/60/24
+#define PATH_SIZE 1024
 
-struct tm*
-now()
-{
-  time_t t = time(NULL);
-  return localtime(&t);
-}
-
-time_t
-midnight()
-{
-  struct tm *t = now();
-  t->tm_hour = 0;
-  t->tm_min = 0;
-  t->tm_sec = 0;
-  return mktime(t);
-}
-
-const char*
-env(const char *name, const char *alt)
-{
-  const char *value = getenv(name);
-  return value ? value : alt;
-}
+const char* env(const char *, const char *);
+time_t midnight();
+struct tm* now();
 
 int
 main(int argc, char *argv[])
 {
-  int ch, advance = 0, backward = DAYS(14);
+  int ch;
+  time_t today = midnight();
+  int backward = 0;
+  int advance = DAYS(14);
+
+  char path[PATH_SIZE];
   const char *editor = env("EDITOR", "vi");
-  const char *calendarfile = env("CALENDAR_FILE", "/home/mat/CALENDAR");
+  const char *calendarfile = "/var/CALENDAR";
+
+  FILE *stream;
+  const char *errstr;
   char *line = NULL;
   size_t linelen = 0;
   ssize_t nread;
-  FILE *stream;
-  time_t today = midnight();
-  int thisyear = now()->tm_year;
   struct tm day = {};
+  int thisyear = now()->tm_year;
   time_t date;
   char *n;
+
+  if (snprintf(path, PATH_SIZE, "%s/%s", getenv("HOME"), "CALENDAR")) {
+    calendarfile = env("CALENDAR_FILE", path);
+  }
 
   while ((ch = getopt(argc, argv, "A:B:t:f:eh")) != -1) {
     switch (ch) {
       case 't':
-        if (strptime(optarg, "%Y-%m-%d", &day) != 0) {
+        if (strptime(optarg, "%Y-%m-%d", &day) != NULL) {
           today = mktime(&day);
         }
         break;
       case 'A':
-        advance = DAYS(strtoimax(optarg, NULL, 10));
+        advance = DAYS(strtonum(optarg, 0, DAYS_MAX, &errstr));
         break;
       case 'B':
-        backward = DAYS(strtoimax(optarg, NULL, 10));
+        backward = DAYS(strtonum(optarg, 0, DAYS_MAX, &errstr));
         break;
       case 'f':
         calendarfile = optarg;
@@ -69,7 +59,7 @@ main(int argc, char *argv[])
         execlp(editor, editor, calendarfile, NULL);
         break;
       default:
-        fputs("usage: agenda [-A num] [-B num] [-t date] [-f file]\n", stderr);
+        fputs("usage: agenda [-A num] [-B num] [-t date] [-f file] [-e]\n", stderr);
         return EXIT_FAILURE;
     }
   }
@@ -82,10 +72,9 @@ main(int argc, char *argv[])
   }
 
   while ((nread = getline(&line, &linelen, stream)) != -1) {
-    if ((n = strptime(line, "%m-%d", &day)) != NULL) {
+    if ((n = strptime(line, "%F", &day)) != NULL) {
+    } else if ((n = strptime(line, "%m-%d", &day)) != NULL) {
       day.tm_year = thisyear;
-    } else if ((n = strptime(line, "%F", &day)) != NULL) {
-      ;
     }
     if (n) {
       date = mktime(&day);
@@ -94,8 +83,36 @@ main(int argc, char *argv[])
       }
     }
   }
+
   free(line);
   fclose(stream);
   return EXIT_SUCCESS;
+}
+
+
+const char*
+env(const char *name, const char *alt)
+{
+  const char *value = getenv(name);
+  return value ? value : alt;
+}
+
+
+time_t
+midnight()
+{
+  struct tm *t = now();
+  t->tm_hour = 0;
+  t->tm_min = 0;
+  t->tm_sec = 0;
+  return mktime(t);
+}
+
+
+struct tm*
+now()
+{
+  time_t t = time(NULL);
+  return localtime(&t);
 }
 
