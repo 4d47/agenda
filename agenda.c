5e8b2d7d1e3181c8a143c5941a9e60e0e25194c6
Mathieu Gagnon <mathieu@gagnon.name>
2018-06-02 11:57:08 -0400

    Adjust to style(9)

--- agenda.c
+++ agenda.c
@@ -1,126 +1,125 @@
 
 #define _XOPEN_SOURCE 700
 
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
 #include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 #define DAYS(n) 60*60*24*n
 #define DAYS_MAX INT_MAX/60/60/24
 #define PATH_SIZE 1024
 #define LINE_LEN 2048 + 1
+#define USAGE "usage: %s [-e] [-f file] [-t date] [-A num] [-B num]\n"
 
-const char* env(const char *, const char *);
-time_t midnight(struct tm *);
-struct tm* now();
+static const char*	env(const char *, const char *);
+static time_t		midnight(struct tm *);
+static struct tm*	now();
 
 int
 main(int argc, char *argv[])
 {
-  int ch;
-  time_t today = midnight(now());
-  int backward = 0;
-  int advance = DAYS(14);
+	int ch;
+	time_t today;
+	int backward = 0;
+	int advance = DAYS(14);
 
-  char path[PATH_SIZE];
-  const char *editor = env("EDITOR", "vi");
-  const char *agendafile = "/var/AGENDA";
+	char path[PATH_SIZE];
+	const char *editor;
+	const char *agendafile;
 
-  FILE *stream;
-  char line[LINE_LEN]; /* *p; */
-  struct tm day = { 0 };
-  int thisyear = now()->tm_year;
-  time_t date;
-  char *n;
+	FILE *stream;
+	char line[LINE_LEN]; /* *p; */
+	struct tm day = { 0 };
+	int thisyear = now()->tm_year;
+	time_t date;
+	char *n;
 
-  if (snprintf(path, PATH_SIZE, "%s/%s", getenv("HOME"), "AGENDA")) {
-    agendafile = env("AGENDA_FILE", path);
-  }
+	today = midnight(now());
+	editor = env("EDITOR", "vi");
+	if (snprintf(path, PATH_SIZE, "%s/%s", getenv("HOME"), "AGENDA"))
+		agendafile = env("AGENDA_FILE", path);
 
-  while ((ch = getopt(argc, argv, "A:B:t:f:eh")) != -1) {
-    switch (ch) {
-      case 't':
-        if (strptime(optarg, "%F", &day)) {
-          today = midnight(&day);
-        }
-        break;
-      case 'A':
-        advance = DAYS(atoi(optarg));
-        break;
-      case 'B':
-        backward = DAYS(atoi(optarg));
-        break;
-      case 'f':
-        agendafile = optarg;
-        break;
-      case 'e':
-        execlp(editor, editor, agendafile, NULL);
-        break;
-      default:
-        fputs("usage: agenda [-e] [-f file] [-t date] [-A num] [-B num]\n", stderr);
-        return EXIT_FAILURE;
-    }
-  }
+	while ((ch = getopt(argc, argv, "A:B:t:f:eh")) != -1) {
+		switch (ch) {
+			case 'e':
+				execlp(editor, editor, agendafile, NULL);
+				break;
+			case 'f':
+				agendafile = optarg;
+				break;
+			case 't':
+				if (strptime(optarg, "%F", &day))
+					today = midnight(&day);
+				break;
+			case 'A':
+				advance = DAYS(atoi(optarg));
+				break;
+			case 'B':
+				backward = DAYS(atoi(optarg));
+				break;
+			default:
+				fprintf(stderr, USAGE, argv[0]);
+				return EXIT_FAILURE;
+		}
+	}
 
-  stream = fopen(agendafile, "r");
+	stream = fopen(agendafile, "r");
 
-  if (errno != 0) {
-    perror("error");
-    return EXIT_FAILURE;
-  }
+	if (errno != 0) {
+		perror("error");
+		return EXIT_FAILURE;
+	}
 
-  while (fgets(line, LINE_LEN, stream) != NULL) {
-    if ((n = strptime(line, "%F", &day))) {
-      ;
-    } else if ((n = strptime(line, "%m-%d", &day))) {
-      day.tm_year = thisyear;
-    }
-    if (n) {
-      date = midnight(&day);
-      if (today - backward <= date && date <= today + advance) {
-        fputs(line, stdout);
+	while (fgets(line, LINE_LEN, stream) != NULL) {
+		if ((n = strptime(line, "%F", &day)))
+			;
+		else if ((n = strptime(line, "%m-%d", &day)))
+			day.tm_year = thisyear;
 
-        /* if ((p = strchr(line, '\n')) == NULL) { */
-        /*   while ((ch = getw(stream)) != '\n' && ch != EOF) { */
-        /*     // ignore rest of line */
-        /*     ; */
-        /*   } */
-        /* } */
-      }
-    }
-  }
+		if (n) {
+			date = midnight(&day);
+			if (today - backward <= date && date <= today + advance)
+				fputs(line, stdout);
 
-  fclose(stream);
-  return EXIT_SUCCESS;
+			/* if ((p = strchr(line, '\n')) == NULL) { */
+			/*   while ((ch = getw(stream)) != '\n' && ch != EOF) { */
+			/*     // ignore rest of line */
+			/*     ; */
+			/*   } */
+			/* } */
+		}
+	}
+
+	fclose(stream);
+	return EXIT_SUCCESS;
 }
 
 
 const char*
 env(const char *name, const char *alt)
 {
-  const char *value = getenv(name);
-  return value ? value : alt;
+	const char *value = getenv(name);
+	return value ? value : alt;
 }
 
 
 time_t
 midnight(struct tm *t)
 {
-  t->tm_hour = 0;
-  t->tm_min = 0;
-  t->tm_sec = 0;
-  t->tm_isdst = 0;
-  return mktime(t);
+	t->tm_hour = 0;
+	t->tm_min = 0;
+	t->tm_sec = 0;
+	t->tm_isdst = 0;
+	return mktime(t);
 }
 
 
 struct tm*
 now()
 {
-  time_t t = time(NULL);
-  return localtime(&t);
+	time_t t = time(NULL);
+	return localtime(&t);
 }
-
